1b214e428e9ad2a0d4b20a10929ab37d706b0bf7
Dilan Shanuka <99383107+IT21182914@users.noreply.github.com>
2024-01-13 21:14:03 +0530

    Finished

--- src/main/java/usermanagement/model/User.java
+++ src/main/java/usermanagement/model/User.java
@@ -1,11 +1,26 @@
 package usermanagement.model;
 
+/**
+ * User.java
+ * This is a model class represents a User entity
+ * @author Ramesh Fadatare
+ *
+ */
 public class User {
-
-	private int id;
-	private String name;
-	private String email;
-	private String country;
+	protected int id;
+	protected String name;
+	protected String email;
+	protected String country;
+	
+	public User() {
+	}
+	
+	public User(String name, String email, String country) {
+		super();
+		this.name = name;
+		this.email = email;
+		this.country = country;
+	}
 
 	public User(int id, String name, String email, String country) {
 		super();
@@ -14,48 +29,29 @@ public class User {
 		this.email = email;
 		this.country = country;
 	}
-	
-	
-
-	public User(String name, String email, String country) {
-		super();
-		this.name = name;
-		this.email = email;
-		this.country = country;
-	}
-
-
 
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getName() {
 		return name;
 	}
-
 	public void setName(String name) {
 		this.name = name;
 	}
-
 	public String getEmail() {
 		return email;
 	}
-
 	public void setEmail(String email) {
 		this.email = email;
 	}
-
 	public String getCountry() {
 		return country;
 	}
-
 	public void setCountry(String country) {
 		this.country = country;
 	}
-
 }
